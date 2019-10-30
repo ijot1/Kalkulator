292e5e0533ca3a504dabe81f66080fada070f294
ijot1 <ireneuszjaneczek128@gmail.com>
2019-10-30 19:37:18 +0100

    Initial commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,35 @@
+package com.kodilla;
+
+public class Calculator {
+    private int firstNumber;
+    private int secondNumber;
+
+
+    public Calculator(int i1, int i2) {
+        firstNumber = i1;
+        secondNumber = i2;
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public int addNumbers() {
+        int sum = firstNumber + secondNumber ;
+        return sum;
+    }
+
+    public int multiplyNumbers() {
+        int product = firstNumber + secondNumber;
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(2, 3);
+        calculator
+    }
+}
---
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -0,0 +1,16 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.*;
+import java.io.*;
+
+@SpringBootApplication
+public class KodillaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseApplication.class, args);
+        }
+
+
+}
